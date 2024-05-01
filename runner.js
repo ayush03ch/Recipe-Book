13a77c7e50f482bc988de3e05e350082b7771284
Ayush Chaurasiya <114729778+ayush03ch@users.noreply.github.com>
2024-05-01 13:28:04 +0530

    Update runner.js

--- runner.js
+++ runner.js
@@ -2,15 +2,27 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const publicDirectory = path.join(__dirname, 'public');
+// paths to static files 
+const cssPath = path.join(__dirname, 'css');
+const jsPath = path.join(__dirname, 'js');
+const assetsPath = path.join(__dirname, 'assets');
 
-// middleware to serve static files
-app.use(express.static(publicDirectory));
+app.use('/css', express.static(cssPath));
+app.use('/js', express.static(jsPath));
+app.use('/assets', express.static(assetsPath));
 
+// Serve HTML files
+app.get('/:pageName', (req, res) => {
+    const pageName = req.params.pageName;
+    const filePath = path.join(__dirname, `${pageName}`);
+    res.sendFile(filePath);
+});
+
+// Serve index page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(8000, () => {
-    console.log('Server is running on port 8000');
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
 });
